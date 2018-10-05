9de50eb4328f67caa52e7e257a758ef552f4cba7
Rafif Shaputra <rafifshaputra25@gmail.com>
2018-10-05 22:53:14 +0700

    Revisi Template

--- src/main/java/com/apap/tutorial4/controller/PilotController.java
+++ src/main/java/com/apap/tutorial4/controller/PilotController.java
@@ -19,63 +19,61 @@ import com.apap.tutorial4.service.PilotService;
 public class PilotController {
 	@Autowired
 	private PilotService pilotService;
-	
-	@RequestMapping("/")private String home() {
+
+	@RequestMapping("/")
+	private String home() {
 		return "home";
 	}
-	
-	@RequestMapping(value="/pilot/add", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/pilot/add", method = RequestMethod.GET)
 	private String add(Model model) {
 		model.addAttribute("pilot", new PilotModel());
 		return "addPilot";
 	}
-	
-	@RequestMapping(value="/pilot/add", method=RequestMethod.POST)
+
+	@RequestMapping(value = "/pilot/add", method = RequestMethod.POST)
 	private String addPilotSubmit(@ModelAttribute PilotModel pilot) {
 		pilotService.addPilot(pilot);
 		return "add";
 	}
-	
+
 	@RequestMapping("/pilot/view")
 	public String view(@RequestParam("licenseNumber") String licenseNumber, Model model) {
 		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
-		
-		List<FlightModel> flight= archive.getPilotFlight();
-		
-		model.addAttribute("pilot", archive);
-		model.addAttribute("flight", flight);
-		return "view-pilot";
+
+		if (archive == null) {
+			return "error-message";
+		}
+
+		else {
+			List<FlightModel> flight = archive.getPilotFlight();
+
+			model.addAttribute("pilot", archive);
+			model.addAttribute("flight", flight);
+			return "view-pilot";
+		}
 	}
-	
-	@RequestMapping(value="/pilot/delete/{id}", method=RequestMethod.GET)
-	private String deletePilot(@PathVariable(value="id")String id, Model model){	
-		
+
+	@RequestMapping(value = "/pilot/delete/{id}", method = RequestMethod.GET)
+	private String deletePilot(@PathVariable(value = "id") String id, Model model) {
+
 		pilotService.deletePilot(Long.parseLong(id));
 		return "delete";
 	}
-	
-	@RequestMapping(value="/pilot/update/{licenseNumber}", method=RequestMethod.GET)
-	private String updatePilot(@PathVariable(value="licenseNumber")String licenseNumber, Model model){	
-		
-		PilotModel archive =pilotService.getPilotDetailByLicenseNumber(licenseNumber);
+
+	@RequestMapping(value = "/pilot/update/{licenseNumber}", method = RequestMethod.GET)
+	private String updatePilot(@PathVariable(value = "licenseNumber") String licenseNumber, Model model) {
+
+		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
 		model.addAttribute("pilot", archive);
 		return "updatePilot";
 	}
-	
-	@RequestMapping(value="/pilot/update/{licenseNumber}/succes", method=RequestMethod.POST)
-	private String updatePilotSubmit(@PathVariable(value="licenseNumber")String licenseNumber, @ModelAttribute PilotModel newPilot ,Model model) {
+
+	@RequestMapping(value = "/pilot/update/{licenseNumber}/succes", method = RequestMethod.POST)
+	private String updatePilotSubmit(@PathVariable(value = "licenseNumber") String licenseNumber,
+			@ModelAttribute PilotModel newPilot, Model model) {
 		pilotService.updatePilot(newPilot, licenseNumber);
 		return "update";
 	}
-	
-	
-	
-	
-	
-	
-	
-		
-	
-	
-	
+
 }
